37a83d508b5ccf6f55ae3614e64d7ffe97e7b2df
Daniel Kelly <me@danielkelly.io>
2023-03-07 11:58:29 -0600

    default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="font-bold text-4xl">Welcome to My Store</div>
-    <NuxtLink to="/products">View Products</NuxtLink>
+    <NuxtLink class="btn mt-5" to="/products">View Products</NuxtLink>
   </div>
 </template>
 
