325e1c0844bbd1fe09db5d68cfa0850d45c06b47
Daniel Kelly <me@danielkelly.io>
2023-03-07 11:55:07 -0600

    installed tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <div class="font-bold text-4xl">Welcome to My Store</div>
+    <NuxtLink to="/products">View Products</NuxtLink>
+  </div>
 </template>
 
 <script>
